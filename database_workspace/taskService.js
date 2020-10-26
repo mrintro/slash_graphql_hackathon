e5b1ae9e09a47b9e654922ba93cf2b3a534a0d7c
Shashank2409 <shashankgupta588@gmail.com>
2020-10-27 01:12:26 +0530

    Queries for user task

---
+++ database_workspace/taskService.js
@@ -0,0 +1,59 @@
+const AddTask = async function(){
+
+}
+
+const GetMyTasks = async function(){
+
+}
+
+const GetMyActiveTasks = async function(){
+
+}
+
+const GetMyClosedTasks = async function(){
+
+}
+
+const GetFriendsActiveTasks = async function(){
+
+}
+
+const GetTasksUsingUserId = async function(){
+
+}
+
+const RemoveTask = async function(){
+
+}
+
+const UpdateTask = async function(){
+
+}
+
+const UpdateTaskStatus = async function(){
+
+}
+
+const ViewTaskStatusUsingTaskId = async function(){
+
+}
+
+const AssignTask = async function(){
+
+}
+
+const ViewAssignedTasks = async function(){
+
+}
+
+const VolunteerTask = async function(){
+
+}
+
+const BackoutFromTask = async function(){
+
+}
+
+const ViewVolunteeredTasks = async function(){
+    
+}
