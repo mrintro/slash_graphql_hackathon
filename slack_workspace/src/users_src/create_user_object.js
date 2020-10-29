371dc145844980ebed0815456c1c4cd64bac07b0
Shashank2409 <shashankgupta588@gmail.com>
2020-10-29 21:04:17 +0530

    create_user_object done

--- slack_workspace/src/users_src/create_user_object.js
+++ slack_workspace/src/users_src/create_user_object.js
@@ -1,35 +1,40 @@
 module.exports = function (details){
     let buttons = details.buttons;
-    let button, obj=[];
+    let button, obj=[] , count = 0;
     for(button of buttons){
+        var color;
+        if(button.action == "acceptFriendRequest" || button.action == "acceptBestFriendRequest"){
+            color = "primary";
+        }
+        else if(button.action == "cancelFriendRequest" || button.action == "sendFriendRequest"){
+            color = "default";
+        }
+        else{
+            color = "danger";
+        }
         obj.push({
-                "type" : "button",
-                "text" : {
-                    "type" : "plain_text",
-                    "emoji": true,
-                    "text" : button.name
-                },
-                "style" : "primary",
-                "value" : button.action+" "+details.user_id
+                "name": "accept_tos",
+                "text": button.name,
+                "type": "button",
+                "value": button.action,
+                "style": color
         })
     }
     
     return [{
-            "type" : "section",
-            "fields" : [
-                {
-                    "type": "mrkdwn",
-                    "text": `*${details['username']}*\n${details['email']}`
-                }
-            ]
-        },
-        {
-            "type" : "actions",
-            "elements" : obj
-        },
-        {
-            "type" : "divider"
+        "text": "Search results for " + details.name,
+        "attachments": [{
+            "text": details.username + " (" + details.name + ")",
+			"thumb_url" : details.profile_picture  ,
+            "fallback": "Unable to search user",
+            "callback_id": "user_found",
+            "color": "#000000",
+            "attachment_type": "default",
+            "actions" : obj
         }
+            
+        ]
+    }
     
     ]
 }
